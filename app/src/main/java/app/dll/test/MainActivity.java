66d376620bfa1e362faf8233f4778c457739b600
Your name <Your email>
2024-03-05 22:30:01 +0200

    Button is working finally!

--- app/src/main/java/app/dll/test/MainActivity.java
+++ app/src/main/java/app/dll/test/MainActivity.java
@@ -1,7 +1,11 @@
 package app.dll.test;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.enter_btn); // Find button after inflating layout
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainMenu.class);
+                startActivity(intent);
+
+            }
+        });
     }
+
 
 
 
--- app/src/main/java/app/dll/test/MainMenu.java
+++ app/src/main/java/app/dll/test/MainMenu.java
@@ -1,14 +1,18 @@
-package app.dll.test;
+package app.dll.test; // Consider a more descriptive package name
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+
     }
 }

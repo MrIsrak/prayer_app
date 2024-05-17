65d2cf20082ad8afc3bc983bb7f4114431364f51
Your name <Your email>
2024-05-17 15:16:36 +0300

    Bottom menu synchronized

--- app/src/main/java/app/dll/test/Main_menu_button_fragment.java
+++ app/src/main/java/app/dll/test/Main_menu_button_fragment.java
@@ -9,11 +9,15 @@ import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDeepLinkBuilder;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class Main_menu_button_fragment extends Fragment {
@@ -32,46 +36,25 @@ public class Main_menu_button_fragment extends Fragment {
         Button tehilimButton = view.findViewById(R.id.tehelim_button);
         Button blessinButton = view.findViewById(R.id.blessings_button);
 
-
-
         NavController navController = Navigation.findNavController(getActivity().findViewById(R.id.nav_host_fragment));
 
-//TODO: After switching the fragment previous one does not hide!!
         timetableButton.setOnClickListener(v -> navController.navigate(R.id.action_main_menu_button_to_timetable_fragment));
         textPrayersButton.setOnClickListener(v -> navController.navigate(R.id.action_main_menu_button_to_textPrayers_fragment));
         tehilimButton.setOnClickListener(v -> navController.navigate(R.id.action_main_menu_button_to_tehilim_fragment));
         blessinButton.setOnClickListener(v -> navController.navigate(R.id.action_main_menu_button_to_blessings_fragment));
 
-//        time_table.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {Navigation.findNavController(view).navigate(R.id.action_main_menu_button_to_timetable_fragment);}
-//
-//        });
+        BottomNavigationView navView = getActivity().findViewById(R.id.bottom_navigation);
 
-//        text_prayers.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) { replaceFragment(new TextPrayers_fragment());}
-//        });
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top-level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.timetable_fragment, R.id.main_menu_button, R.id.tehilim_fragment)
+                .build();
 
-//        tehilim.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {replaceFragment(new Tehilim_fragment());}
-//        });
-
-//        blessings.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {replaceFragment(new Blessings_fragment());}
-//        });
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
 
 
-//    public void replaceFragment(Fragment fragment){
-//        //replace the fragment with actual class name
-//        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-//        FragmentTransaction transaction = fragmentManager.beginTransaction();
-//        Fragment timetableFragment = new Timetable_fragment();
-//        transaction.replace(R.id.menu_buttons, timetableFragment);
-//        transaction.commit();
-//    }
     }
 
 }
